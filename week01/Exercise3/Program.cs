fa0854d97b2f07c3ad9bd82e9b45623ea75534cf
otuekongakpan <oakpan2@byupathway.edu>
2025-10-29 18:21:12 +0100

    Exercise 3 done

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,47 @@
 using System;
+using System.Net;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+
+        int guess = -1;
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int count = 0;
+        string response = "yes";
+
+        while (response == "yes")
+        {
+            while (guess != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                    count += 1;
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                    count += 1;
+                }
+                else
+                {
+                    count += 1;
+                    Console.WriteLine("You guessed it!");
+                    Console.WriteLine($"You tried {count} times");
+
+                }
+
+
+
+            }
+        }
     }
 }
