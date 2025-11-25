8a3f9c878a965ca2efa4b4bf904271a8ad9990b9
otuekongakpan <oakpan2@byupathway.edu>
2025-11-25 17:07:20 +0100

    Homework done

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        MathAssignment a1 = new MathAssignment("Matthew", "Algebra", "12.7", "20-32");
+
+        Console.WriteLine(a1.GetSummary());
+        Console.WriteLine(a1.GetHomeworkList());
+
+        WritingAssignment a2 = new WritingAssignment("Michelle", "Mild Expectations", "The Beginnning of the End");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetWritingInformation());
     }
 }
